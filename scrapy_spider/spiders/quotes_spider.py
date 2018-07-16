02d9b4bc89e31156fa5f7770c67a1d341ecc9f79
abhilash <abhilashthot99@gmail.com>
2018-07-16 18:47:37 +0530

    scrapy working

--- scrapy_spider/spiders/quotes_spider.py
+++ scrapy_spider/spiders/quotes_spider.py
@@ -5,6 +5,7 @@ import scrapy
 class QuotesSpiderSpider(scrapy.Spider):
     name = 'quotes_spider'
     ratings=[]
+    i=0
     allowed_domains = ['www.booking.com']
     start_urls = ['https://www.booking.com/searchresults.en-gb.html?city=-2106102']
 
@@ -14,13 +15,19 @@ class QuotesSpiderSpider(scrapy.Spider):
         results = links[:5]
         base_url = 'https://www.booking.com/'
         for link in results:
-        	full_url = base_url+ link[2:]
-        	print(full_url)
-        	res = scrapy.Request(full_url,callback = self.parse_hotel)
-        print(self.ratings)
+            full_url = base_url+ link[2:]
+            yield scrapy.Request(full_url,callback = self.parse_hotel)
+        
 
     def parse_hotel(self,response):
-    	print("here")
-    	name=response.xpath("//h2[@id='hp_hotel_name']/text()")
-    	rating = response.xpath("//div[@id='js--hp-gallery-scorecard']/@data-review-score")
-    	self.ratings.append((name,rating))
+        name=response.xpath("//h2[@id='hp_hotel_name']/text()").extract()
+        rating = response.xpath("//div[@id='js--hp-gallery-scorecard']/@data-review-score").extract()
+        self.ratings.append((name,rating))
+        self.i+=1
+        self.print_ratings()
+
+    def print_ratings(self):
+    	if(self.i==5):
+        	print("\n hotels and ratings")
+        	for rating in self.ratings:
+        		print(" {} : {} ".format(rating[0],rating[1]))
